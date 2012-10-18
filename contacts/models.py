2fd8723ae23562a1fe0d3fb405ea20342e41dc6c
Julian Threatt <porfirio84@gmail.com>
2012-10-17 17:19:19 -0700

    inital commit

---
+++ contacts/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.localflavor.us.models import PhoneNumberField
+# Create your models here.
+from djangorestframework.resources import ModelResource
+
+PHONE_NUMBER_CHOICES = (
+    (u'1', 'Home'),
+    (u'2', 'Work'),
+    (u'3', 'Mobile')
+)
+
+class Person(models.Model):
+    fb_id = models.CharField(max_length=60)
+    first = models.CharField(max_length=255)
+    last = models.CharField(max_length=255)
+    lkdin_id = models.CharField(max_length=255)
+    nick_name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos') #avatar
+    emails = models.ForeignKey('EmailAddresses')
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    persons = models.ManyToManyField(Person)
+
+class Event(models.Model):
+    attendees = models.ManyToManyField(Person)
+    groups = models.ManyToManyField(Group)
+    uuid = models.CharField(max_length=255)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    # TODO (julian) there maybe more fields we need
+
+class EmailAddresses(models.Model):
+    primary = models.EmailField()
+    secondary = models.EmailField()
+    ternary = models.EmailField()
+
+class PhoneNumber(models.Model):
+    type = models.CharField(max_length=1, choices=PHONE_NUMBER_CHOICES)
+    phone = PhoneNumberField()
+
+class ContactsResource(ModelResource):
+    model = Person
---
+++ mozzieserver/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from contacts.models import ContactsResource
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mozzieserver.views.home', name='home'),
+    # url(r'^mozzieserver/', include('mozzieserver.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('djangorestframework.urls', namespace='djangorestframework')),
+    url(r'^people/$', ListOrCreateModelView.as_view(resource=ContactsResource)),
+    url(r'^(?P<pk>[d]+)/$', InstanceModelView.as_view(resource=ContactsResource)),
+)
